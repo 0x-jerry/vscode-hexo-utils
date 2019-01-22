e843986dd7b79e2f7ed2e40c921e85cc7ee0ce77
cwxyz007 <jie844067636@gmail.com>
2019-01-22 12:13:38 +0800

    feat: Support delete the asset folder when empty

--- src/commands/deleteFile.ts
+++ src/commands/deleteFile.ts
@@ -2,6 +2,8 @@ import { error } from '../utils';
 import { ArticleItem } from '../hexoArticleProvider';
 import { Command, ICommandParsed, Commands, command } from './common';
 import * as fs from 'fs-extra';
+import * as path from 'path';
+import { configs } from '../configs';
 
 @command()
 export class DeleteFile extends Command {
@@ -11,10 +13,19 @@ export class DeleteFile extends Command {
 
   async execute(cmd: ICommandParsed, item: ArticleItem): Promise<any> {
     const filePath = item.resourceUri!.fsPath;
+    const filename = path.parse(filePath).name;
     if (await fs.pathExists(filePath)) {
       await fs.unlink(filePath);
+      await this.deleteAssetFolder(filename);
     } else {
       error(`${filePath} is not exist`);
+    }
+  }
+
+  async deleteAssetFolder(filename: string) {
+    const assetsFolder = path.join(configs.paths.post, filename);
+    if ((await fs.pathExists(assetsFolder)) && (await fs.readdir(assetsFolder)).length === 0) {
+      await fs.rmdir(assetsFolder);
     }
   }
 }
