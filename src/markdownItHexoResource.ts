2ef7e15ea7be04c3df17cb654dfd72083aec0a97
cwxyz007 <jie844067636@gmail.com>
2019-01-25 10:42:35 +0800

    refactor: Refactor preview hexo markdown

--- src/markdownItHexoResource.ts
+++ src/markdownItHexoResource.ts
@@ -2,81 +2,102 @@ import Token = require('markdown-it/lib/token');
 import * as MarkdownIt from 'markdown-it';
 import * as path from 'path';
 import { window, workspace } from 'vscode';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 
-export default function plugin(md: MarkdownIt) {
-  md.core.ruler.after('inline', 'hexo-resource', (status) => {
-    status.tokens.forEach((token) => {
-      if (!token.children) {
-        return;
-      }
+class MarkdownHexoPlugin {
+  md: MarkdownIt;
 
-      token.children.forEach((child) => {
-        if (child.type === 'image') {
-          hexoImage(child);
-        }
-        if (child.type === 'text') {
-          hexoTag(child, md);
-        }
-      });
-    });
-  });
-}
+  static vsResPrefix = 'vscode-resource:/';
 
-function hexoImage(token: Token) {
-  // vscode-resource:/c:xx/xx/xx.png
-  const oldSrc = token.attrGet('src');
+  constructor(md: MarkdownIt) {
+    this.md = md;
+    this.install();
+  }
 
-  if (window.activeTextEditor && workspace.rootPath && oldSrc) {
-    const prefix = 'vscode-resource:/';
-    const filePath = window.activeTextEditor.document.uri.fsPath;
-
-    const srcUri = oldSrc.substr(prefix.length);
-    // c:/xxx/xx/source/_posts/xxx/xxx.png => xxx/xxx.png
-    const srcName = srcUri.substr(path.dirname(filePath).length + 1);
-
+  private getResDir(filePath: string) {
     const resourceDir = path.join(
-      workspace.rootPath,
+      workspace.rootPath!,
       'source',
       '_posts',
       path.parse(filePath).name,
     );
+    return resourceDir;
+  }
 
-    if (!fs.existsSync(srcUri) && fs.existsSync(path.join(resourceDir, srcName))) {
-      token.attrSet('src', prefix + path.join(resourceDir, srcName));
+  install() {
+    this.md.core.ruler.after('inline', 'hexo-resource', (status) => {
+      status.tokens.forEach((token) => {
+        if (!token.children) {
+          return;
+        }
+
+        token.children.forEach((child) => {
+          // ![img](img.png)
+          if (child.type === 'image') {
+            this.hexoImage(child);
+          }
+          // {% assert_img img.png alt title %}
+          if (child.type === 'text') {
+            this.hexoTag(child);
+          }
+        });
+      });
+    });
+  }
+
+  hexoImage(token: Token) {
+    // vscode-resource:/c:xx/xx/xx.png
+    const oldSrc = token.attrGet('src');
+
+    if (window.activeTextEditor && workspace.rootPath && oldSrc) {
+      const prefix = MarkdownHexoPlugin.vsResPrefix;
+      const filePath = window.activeTextEditor.document.uri.fsPath;
+
+      const srcUri = oldSrc.substr(prefix.length);
+      // c:/xxx/xx/source/_posts/xxx/xxx.png => xxx/xxx.png
+      const srcName = srcUri.substr(path.dirname(filePath).length + 1);
+
+      const resourceDir = this.getResDir(filePath);
+
+      if (!fs.existsSync(srcUri) && fs.existsSync(path.join(resourceDir, srcName))) {
+        token.attrSet('src', prefix + path.join(resourceDir, srcName));
+      }
     }
   }
-}
 
-// {% asset_img img.png this is a img %}
-function hexoTag(token: Token, md: MarkdownIt) {
-  const imgReg = /{%(.+)?%}/.exec(token.content);
-  if (imgReg && imgReg[1]) {
+  hexoTag(token: Token) {
+    const imgReg = /{%(.+)?%}/.exec(token.content);
+    if (!(imgReg && imgReg[1])) {
+      return;
+    }
+
     //[asset_img, img.png, this, is, a, img]
     const attrs = imgReg[1].split(/\s+/).filter((s) => !!s);
     if (attrs.length < 2) {
       return;
     }
 
-    const [alt, src] = attrs;
+    const [tag, src, ...alt] = attrs;
 
-    const prefix = 'vscode-resource:/';
-    if (window.activeTextEditor && workspace.rootPath) {
-      const filePath = window.activeTextEditor.document.uri.fsPath;
-      const resourceDir = path.join(
-        workspace.rootPath,
-        'source',
-        '_posts',
-        path.parse(filePath).name,
-      );
-
-      const vscodeSrc = prefix + path.join(resourceDir, src);
-
-      token.type = 'image';
-      token.tag = 'img';
-      token.attrs = [['src', vscodeSrc], ['alt', alt]];
-      token.content = `![${alt}](${src})`;
-      token.children = md.parseInline(alt, null);
+    if (tag === 'asset_img') {
+      this.createHexoImgTag(token, src, alt.join(' '));
     }
   }
+
+  createHexoImgTag(token: Token, src: string, alt: string) {
+    const prefix = MarkdownHexoPlugin.vsResPrefix;
+    const filePath = window.activeTextEditor!.document.uri.fsPath;
+    const resourceDir = this.getResDir(filePath);
+
+    const localPath = path.join(resourceDir, src);
+    const vscodeSrc = fs.existsSync(localPath) ? prefix + localPath : src;
+
+    token.type = 'image';
+    token.tag = 'img';
+    token.attrs = [['src', vscodeSrc], ['alt', alt]];
+    token.content = `![${alt}](${vscodeSrc})`;
+    token.children = this.md.parseInline(alt, null);
+  }
 }
+
+export default (md: MarkdownIt) => new MarkdownHexoPlugin(md);
