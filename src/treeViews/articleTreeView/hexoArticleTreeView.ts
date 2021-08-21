dbcec57526a4d3b770b4432dfc7dc198e611e6ae
Jerry Wang <x.jerry.wang@gmail.com>
2021-08-21 09:58:36 +0800

    perf: reveal article item when file was opened. close #43

--- src/treeViews/articleTreeView/hexoArticleTreeView.ts
+++ src/treeViews/articleTreeView/hexoArticleTreeView.ts
@@ -2,6 +2,7 @@ import { HexoArticleProvider, ArticleItem } from './hexoArticleProvider';
 import { ArticleTypes, Commands } from '../../commands';
 import { TreeViewOptions, commands, window } from 'vscode';
 import { BaseTreeView, ViewTypes } from '../common';
+import { sleep } from '../../utils';
 
 export class ArticleTreeView extends BaseTreeView<ArticleItem> {
   provider: HexoArticleProvider;
@@ -21,29 +22,39 @@ export class ArticleTreeView extends BaseTreeView<ArticleItem> {
     this.autoFocus();
   }
 
+  private async focus(editor = window.activeTextEditor) {
+    if (!editor || !this.treeView.visible) {
+      return;
+    }
+
+    const file = editor.document.uri;
+
+    const fsPath =
+      file.scheme === 'git'
+        ? // remove `.git` suffix
+          file.fsPath.slice(0, -3)
+        : file.fsPath;
+
+    const item = this.provider.getItem(fsPath);
+
+    if (!item) {
+      // Make sure load all tree view items, then try to focus again.
+      await sleep(300);
+      this.focus();
+      return;
+    }
+
+    this.treeView.reveal(item);
+  }
+
   private autoFocus() {
-    const _dispose = window.onDidChangeActiveTextEditor(async (editor) => {
-      if (!editor || !this.treeView.visible) {
-        return;
-      }
+    this.subscribe(
+      window.onDidChangeActiveTextEditor((editor) => {
+        this.focus(editor);
+      }),
+    );
 
-      const file = editor.document.uri;
-
-      const fsPath =
-        file.scheme === 'git'
-          ? // remove `.git` suffix
-            file.fsPath.slice(0, -3)
-          : file.fsPath;
-
-      const item = this.provider.getItem(fsPath);
-      if (!item) {
-        return;
-      }
-
-      this.treeView.reveal(item);
-    });
-
-    this.subscribe(_dispose);
+    this.subscribe(this.treeView.onDidChangeVisibility(() => this.focus()));
   }
 
   onDidChanged() {
