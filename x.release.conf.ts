0d3932beb9919716b18a39eaf068aef1e43b0993
Jerry Wang <x.jerry.wang@gmail.com>
2024-02-13 14:57:13 +0800

    chore: update release workflow

--- x.release.conf.ts
+++ x.release.conf.ts
@@ -2,8 +2,6 @@ import { defineConfig, InternalReleaseTask } from '@0x-jerry/x-release';
 
 export default defineConfig({
   sequence: [
-    'npm:changelog',
-    'run:git add CHANGELOG.md',
     InternalReleaseTask.updatePkg,
     InternalReleaseTask.commit,
     InternalReleaseTask.tag,
